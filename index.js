bdbfb1ddbcfa7144460024464d0e6f35d59a6e4f
Matthew <phillips.matthew829@gmail.com>
2021-11-03 12:49:24 -0500

    commit tree a528719dad2ac7c2784938433ebabe40316ae291

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(msg = "roller-skate"){
+    return `This Saturday, I want to ${msg}!`;
+}
+
+function mondayWork(msg = "go to the office"){
+    return `This Monday, I will ${msg}.`;
+}
+
+function wrapAdjective(wrap = "*"){
+    return function (adjective = "special"){
+        return `You are ${wrap}${adjective}${wrap}!`;
+    };
+}
